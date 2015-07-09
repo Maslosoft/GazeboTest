96c989dd2c34f89545ede97b1dfc56c479f8132f
Peter Maselkowski <pmaselkowski@gmail.com>
2015-07-09 14:08:46 +0200

    + Tests

--- tests/unit/Factory/InstancesTest.php
+++ tests/unit/Factory/InstancesTest.php
@@ -64,6 +64,29 @@ class InstancesTest extends Test
 		$this->assertSame($plugins[1], $plugins2[1]);
 	}
 
+	public function testIfWillConfigureModelsFromFly()
+	{
+		$model = new TestModel;
+		$plugins = PluginFactory::fly()->instance($this->config, $model);
+		$this->assertSame(3, count($plugins), 'Should create 3 plugin instances');
+
+		// Now configure second instance
+		$plugins2 = PluginFactory::fly()->instance($this->config, $model);
+		$this->assertSame(3, count($plugins2), 'Should create 3 plugin instances');
+
+		// Default config check
+		$expectedCfg = $this->config[TestModel::class];
+
+		$this->assertInstanceOf($expectedCfg[0], $plugins2[0]);
+		$this->assertInstanceOf($expectedCfg[1]['class'], $plugins2[1]);
+		$this->assertInstanceOf($expectedCfg[2], $plugins2[2]);
+		$this->assertSame($expectedCfg[1]['options'], $plugins2[1]->options);
+
+		// Check if same instances
+		$this->assertSame($plugins[0], $plugins2[0]);
+		$this->assertSame($plugins[1], $plugins2[1]);
+	}
+
 	public function testIfWillConfigureModelsWithSelectedInterfaces()
 	{
 		$model = new TestModel;
@@ -92,11 +115,11 @@ class InstancesTest extends Test
 		$this->assertInstanceOf($expectedCfg[0], $plugins2[0]);
 		$this->assertInstanceOf($expectedCfg[1]['class'], $plugins2[1]);
 		$this->assertSame($expectedCfg[1]['options'], $plugins2[1]->options);
-		
+
 		// Check if same instances
 		$this->assertSame($plugins[0], $plugins2[0]);
 		$this->assertSame($plugins[1], $plugins2[1]);
-		
+
 		$this->assertSame($plugins[1], $plugins3[0]);
 		$this->assertSame($plugins2[1], $plugins3[0]);
 	}

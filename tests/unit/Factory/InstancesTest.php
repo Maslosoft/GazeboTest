6dcf02c7454f45169a9ea3cdb950624f568f4006
Peter Maselkowski <pmaselkowski@gmail.com>
2015-11-16 14:44:59 +0100

    + Tests

--- tests/unit/Factory/InstancesTest.php
+++ tests/unit/Factory/InstancesTest.php
@@ -87,6 +87,28 @@ class InstancesTest extends Test
 		$this->assertSame($plugins[1], $plugins2[1]);
 	}
 
+	public function testIfWillConfigureModelsFromFlyWithClassNameParam()
+	{
+		$plugins = PluginFactory::fly()->instance($this->config, TestModel::class);
+		$this->assertSame(3, count($plugins), 'Should create 3 plugin instances');
+
+		// Now configure second instance
+		$plugins2 = PluginFactory::fly()->instance($this->config, TestModel::class);
+		$this->assertSame(3, count($plugins2), 'Should create 3 plugin instances');
+
+		// Default config check
+		$expectedCfg = $this->config[TestModel::class];
+
+		$this->assertInstanceOf($expectedCfg[0], $plugins2[0]);
+		$this->assertInstanceOf($expectedCfg[1]['class'], $plugins2[1]);
+		$this->assertInstanceOf($expectedCfg[2], $plugins2[2]);
+		$this->assertSame($expectedCfg[1]['options'], $plugins2[1]->options);
+
+		// Check if same instances
+		$this->assertSame($plugins[0], $plugins2[0]);
+		$this->assertSame($plugins[1], $plugins2[1]);
+	}
+
 	public function testIfWillConfigureModelsWithSelectedInterfaces()
 	{
 		$model = new TestModel;
